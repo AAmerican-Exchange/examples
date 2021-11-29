9eac206f3d12c86a47d903906c91b314a7c7e003
Justin De Burgo <jpdeburgo.jp@gmail.com>
2021-11-29 12:44:33 -0600

    fixing bs

--- algorithms/iterative_bs.js
+++ algorithms/iterative_bs.js
@@ -1,46 +1,43 @@
 // Program to implement iterative Binary Search
 
-
 // A iterative binary search function. It returns
 // location of x in given array arr[l..r] is present,
 // otherwise -1
-function binarySearch(arr, left, right, target)
-{
-	// your code goes here
-	right = arr.length - 1
-	
-    while (right >= left) {
-        let mid = left + Math.floor((right - left) / 2);
+function binarySearch(arr, left, right, target) {
+  // your code goes here
+  right = arr.length - 1;
 
-        // If the element is present at the middle
-        // itself
-        if (arr[mid] == target) {
+  while (right >= left) {
+    let mid = left + Math.floor((right - left) / 2); //rounds down ex). 3.5 = 3
 
-			return mid;
-        }
-
-        // If element is smaller than mid, then
-        // it can only be present in left subarray
-        if (arr[mid] < target) {
-            left = mid + 1
-        }
-
-        // Else the element can only be present
-        // in right subarray
-        else {
-            right= mid - 1
-        }
+    // If the element is present at the middle
+    // itself
+    if (arr[mid] == target) {
+      return mid;
     }
-	// We reach here when element is not
-	// present in array
-	return -1;
+
+    // If element is smaller than mid, then
+    // it can only be present in left subarray
+    if (arr[mid] < target) {
+      left = mid + 1;
+    }
+
+    // Else the element can only be present
+    // in right subarray
+    else {
+      right = mid - 1;
+    }
+  }
+  // We reach here when element is not
+  // present in array
+  return -1;
 }
 
-	arr = new Array(2, 3, 4, 10, 40);
-	x = 10;
-	n = arr.length;
-	result = binarySearch(arr, 0, n - 1, x);
-	
-(result == -1) ? console.log("Element is not present in array")
-			: console.log("Element is present at index " + result);
-				
+let arr = new Array(2, 3, 4, 10, 40);
+let x = 10;
+let n = arr.length;
+let result = binarySearch(arr, 0, n - 1, x);
+
+result == -1
+  ? console.log("Element is not present in array")
+  : console.log("Element is present at index " + result);
--- algorithms/recursive_bs.js
+++ algorithms/recursive_bs.js
@@ -1,44 +1,41 @@
 // Program to implement recursive Binary Search
 
-
 // A recursive binary search function. It returns
 // location of x in given array arr[l..r] is present,
 // otherwise -1
-function binarySearch(arr, left, right, target)
-{
-	if (right >= left) {
-		mid = left + Math.floor((right - left) / 2);
+function binarySearch(arr, left, right, target) {
+  if (right >= left) {
+    let mid = left + Math.floor((right - left) / 2);
 
-		// If the element is present at the middle
-		// itself
-		if (arr[mid] == target) {
-			return mid;
-		}
+    // If the element is present at the middle
+    // itself
+    if (arr[mid] == target) {
+      return mid;
+    }
 
-		// If element is smaller than mid, then
-		// it can only be present in left subarray
-		if (arr[mid] > target) {
-			return binarySearch(arr, left, mid - 1, target);
-		}
+    // If element is smaller than mid, then
+    // it can only be present in left subarray
+    if (arr[mid] > target) {
+      return binarySearch(arr, left, mid - 1, target);
+    }
 
-		// Else the element can only be present
-		// in right subarray
-		else {
-			return binarySearch(arr, mid + 1, right, target);
-		}
-	}
+    // Else the element can only be present
+    // in right subarray
+    else {
+      return binarySearch(arr, mid + 1, right, target);
+    }
+  }
 
-	// We reach here when element is not
-	// present in array
-	return -1;
+  // We reach here when element is not
+  // present in array
+  return -1;
 }
 
-	arr = new Array(2, 3, 4, 10, 40);
-	x = 10;
-	n = arr.length;
-	result = binarySearch(arr, 0, n - 1, x);
-	
-(result == -1) ? console.log("Element is not present in array")
-			: console.log ("Element is present at index " + result);
-				
+let arr = new Array(2, 3, 4, 10, 40);
+let x = 10;
+let n = arr.length;
+let result = binarySearch(arr, 0, n - 1, x);
 
+result == -1
+  ? console.log("Element is not present in array")
+  : console.log("Element is present at index " + result);
